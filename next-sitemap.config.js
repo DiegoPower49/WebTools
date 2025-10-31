49f56d6005401c396060aef40235eac0b7aacbd7
Diego Arturo Torres Pacherre <datorres@dhmont.local>
2025-10-31 11:11:45 -0500

    configurando sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,8 +3,12 @@ module.exports = {
   siteUrl: "https://fasttools.vercel.app", // tu URL base
   generateRobotsTxt: true, // genera también robots.txt
   sitemapSize: 7000,
-  exclude: ["/404"], // rutas que no quieres indexar
+  exclude: ["/404"], // excluye rutas que no deben indexarse
+  changefreq: "daily",
+  priority: 0.7,
   robotsTxtOptions: {
-    policies: [{ userAgent: "*", allow: "/" }],
+    policies: [
+      { userAgent: "*", allow: "/" }, // permite el rastreo de todo
+    ],
   },
 };
